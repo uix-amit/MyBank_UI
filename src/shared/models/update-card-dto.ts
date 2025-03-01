4aec2cb2ed3965dd03bd1b357eddc20f7f609a1d
uix-amit <amit.uiexplorer@gmail.com>
2025-03-01 21:49:49 +0530

    fix(transactions): fixing build issues for deployment

--- src/shared/models/update-card-dto.ts
+++ src/shared/models/update-card-dto.ts
@@ -45,7 +45,7 @@ export interface UpdateCardDto {
    * @memberof UpdateCardDto
    * @example 2024-07-10T06:52:41.153Z
    */
-  ExpirationDate: Date | String;
+  ExpirationDate: Date | string;
 
   /**
    * @type {number}
