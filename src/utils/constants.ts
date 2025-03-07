013a2c4eef955ca7a9c07406f284358f25bfd878
uix-amit <amit.uiexplorer@gmail.com>
2025-03-07 09:44:41 +0530

    refactor(constants): update BASE_URL to production API endpoint
    base URL updated to use vercel backend app url

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
-export const BASE_URL: string = 'http://localhost:3000/api/v1';
+export const BASE_URL: string = 'https://my-bank-puce.vercel.app/api/v1';
 
 export const LOAN_TYPES = [
   { value: 'HOME', label: 'HOME' },
